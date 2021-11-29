a6c3305667b0c91cdea703de12473766f41cb652
成田幸生 <nknh2823@icloud.com>
2021-11-29 23:45:39 +0900

    triangle

---
+++ Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorSampe
+{
+    class Triangle
+    {
+        private float width; //幅
+        private float height;//高さ
+        public Triangle(float w, float h)
+        {
+            this.width = w;
+            this.height = h;
+        }
+        public float GetSurface()
+        {
+            return (width * height / 2);//面積
+        }
+    }
+}
